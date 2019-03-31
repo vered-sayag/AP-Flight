2cf92bca9b1359159299c21e8c467da9498140c7
vered-sayag <vered8765@gmail.com>
2019-03-31 03:16:02 +0300

    string and print fixd

--- lexer.cpp
+++ lexer.cpp
@@ -14,14 +14,63 @@ vector<string> lexer(string filename) {
     ifstream myfile(filename);
     if (myfile.is_open()) {
         while (getline(myfile, s)) {
-            // Search again and again
-            while (std::regex_search(s, m, r)) {
-                string s2 = m[0];
-                output1.push_back(s2);
 
-                // remove everything until after this match, and search again
-                s = m.suffix().str();
-            }// next match
+            //TODO - strings
+            int i,j,k;
+            int sum =0;
+            for (i = 0; i < s.length(); i++) {
+                if (s[i] == '\"')sum++;
+            }
+
+            if (sum % 2 != 0) {
+                throw invalid_argument("Bad quotation marks!");
+            }
+
+            for (i = 0; i < s.length(); i++) {
+                if (s[i] != ' ')break;
+            }
+            for ( k = i; i < s.length(); ) {
+                if (s[i] == '\"') {
+                    if (i != k) {
+                        output2.push_back(s.substr(k, i - k));
+                        k = i;
+                    }
+
+                    i++;
+                    while (s[i] != '\"') {
+                        i++;
+                    }
+                    output2.push_back(s.substr(k, i + 1 - k));
+                    i++;
+                    k = i;
+
+                }else {
+                   i++;
+                }
+            }
+            if (k!=i){
+                output2.push_back(s.substr(k,i-k));
+            }
+            //end
+
+            // Search again and again
+            for (std::list<string>::iterator it = output2.begin(); it != output2.end(); ++it) {
+                string s1 = (*it);
+
+                if ((regex_match((*it), regex("^\".*\"$")))) {
+                    output1.push_back((*it));
+                    continue;
+                }
+                while (std::regex_search(s1, m, r)) {
+
+                    string s2 = m[0];
+                    output1.push_back(s2);
+
+                    // remove everything until after this match, and search again
+                    s1 = m.suffix().str();
+                }// next match
+            }
+            output2.clear();
 
             for (std::list<string>::iterator op = oper.begin(); op != oper.end(); ++op) {
                 for (std::list<string>::iterator it = output1.begin(); it != output1.end(); ++it) {
@@ -57,7 +106,7 @@ vector<string> lexer(string filename) {
             }
             output1.clear();
         }
-    }else{
+    } else {
         throw invalid_argument("cant open file " + filename);
     }
     myfile.close();
--- printCommand.cpp
+++ printCommand.cpp
@@ -6,27 +6,18 @@ void printCommand::doCommand(int index, vector<string> lex) {
     if (index >= lex.size() - 1) {
         throw invalid_argument("invalid argument to print");
     }
-    if (regex_match(lex[index + 1], regex("^\"[^\"]*\"$"))) {
-        string s = (lex[index + 1]).substr(1, lex[index + 1].size() - 2);
-        cout << s << endl;
-    } else if (data->isSymbol(lex[index + 1])) {
-        cout << data->getSymbolValue(lex[index + 1]) << endl;
-    }else if(regex_match(lex[index + 1], regex("^\"[^\"]*$"))){
-        string s = (lex[index + 1]).substr(1, lex[index + 1].size() - 1);
-        cout << s + " ";
-        index++;
-        while (!regex_match(lex[index + 1], regex("^[^\"]*\"$"))){
-            if (index+1>= lex.size()){
-                throw invalid_argument("invalid argument to print");
-            }
-            cout << lex[index + 1] + " ";
-            index++;
+    index++;
+    do {
+        if (regex_match(lex[index ], regex("^\"[^\"]*\"$"))) {
+            string s = (lex[index ]).substr(1, lex[index ].size() - 2);
+            cout << s ;
+        } else if (data->isSymbol(lex[index])) {
+            cout << data->getSymbolValue(lex[index]) ;
+        } else {
+            throw invalid_argument("invalid argument to print");
         }
-        s = (lex[index + 1]).substr(0, lex[index + 1].size() - 1);
-        cout << s << endl;
-    }
-    else {
-        throw invalid_argument("invalid argument to print");
-    }
+        index++;
+    }while (index+1<lex.size() && (lex[index++]).compare("+")==0);
+    cout << "" << endl;
 
 }
